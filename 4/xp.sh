2b7a73f44ee8920c0250dfc7b3c733700688bd4f
vinstechmy <82468311+vinstechmy@users.noreply.github.com>
2021-10-23 15:08:04 +0800

    Update xp.sh

--- 4/xp.sh
+++ 4/xp.sh
@@ -5,40 +5,6 @@ NC='\e[0m'
 MYIP=$(wget -qO- ifconfig.co);
 echo "Autoscript By Vinstechmy"
 echo ""
-data=( `cat /etc/shadowsocks-libev/akun.conf | grep '^###' | cut -d ' ' -f 2`);
-now=`date +"%Y-%m-%d"`
-for user in "${data[@]}"
-do
-exp=$(grep -w "^### $user" "/etc/shadowsocks-libev/akun.conf" | cut -d ' ' -f 3)
-d1=$(date -d "$exp" +%s)
-d2=$(date -d "$now" +%s)
-exp2=$(( (d1 - d2) / 86400 ))
-if [[ "$exp2" = "0" ]]; then
-sed -i "/^### $user $exp/,/^port_http/d" "/etc/shadowsocks-libev/akun.conf"
-systemctl disable shadowsocks-libev-server@$user-tls.service
-systemctl disable shadowsocks-libev-server@$user-http.service
-systemctl stop shadowsocks-libev-server@$user-tls.service
-systemctl stop shadowsocks-libev-server@$user-http.service
-rm -f "/etc/shadowsocks-libev/$user-tls.json"
-rm -f "/etc/shadowsocks-libev/$user-http.json"
-fi
-done
-data=( `cat /usr/local/shadowsocksr/akun.conf | grep '^###' | cut -d ' ' -f 2`);
-now=`date +"%Y-%m-%d"`
-for user in "${data[@]}"
-do
-exp=$(grep -w "^### $user" "/usr/local/shadowsocksr/akun.conf" | cut -d ' ' -f 3)
-d1=$(date -d "$exp" +%s)
-d2=$(date -d "$now" +%s)
-exp2=$(( (d1 - d2) / 86400 ))
-if [[ "$exp2" = "0" ]]; then
-sed -i "/^### $user $exp/d" "/usr/local/shadowsocksr/akun.conf"
-cd /usr/local/shadowsocksr
-match_del=$(python mujson_mgr.py -d -u "${user}"|grep -w "delete user")
-cd
-fi
-done
-/etc/init.d/ssrmu restart
 data=( `cat /etc/trojan/akun.conf | grep '^###' | cut -d ' ' -f 2`);
 now=`date +"%Y-%m-%d"`
 for user in "${data[@]}"
@@ -107,8 +73,8 @@ d1=$(date -d "$exp" +%s)
 d2=$(date -d "$now" +%s)
 exp2=$(( (d1 - d2) / 86400 ))
 if [[ "$exp2" = "0" ]]; then
-sed -i "s/### $user $exp/### $user $exp4/g" /etc/xray-mini/vless-direct.json
-sed -i "s/### $user $exp/### $user $exp4/g" /etc/xray-mini/vless-splice.json
+sed -i "/^### $user $exp/,/^},{/d" /etc/xray-mini/vless-direct.json
+sed -i "/^### $user $exp/,/^},{/d" /etc/xray-mini/vless-splice.json
 fi
 done
 systemctl restart xray-mini@vless-direct
