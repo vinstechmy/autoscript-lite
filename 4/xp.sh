20e92539ee2f03e0bae7594d5865ee380abed3c3
vinstechmy <82468311+vinstechmy@users.noreply.github.com>
2021-10-23 14:45:32 +0800

    Update xp.sh

--- 4/xp.sh
+++ 4/xp.sh
@@ -98,3 +98,18 @@ fi
 done
 systemctl restart v2ray@vless
 systemctl restart v2ray@vnone
+data=( `cat /etc/xray-mini/vless-direct.json | grep '^###' | cut -d ' ' -f 2`);
+now=`date +"%Y-%m-%d"`
+for user in "${data[@]}"
+do
+exp=$(grep -w "^### $user" "/etc/xray-mini/vless-direct.json" | cut -d ' ' -f 3)
+d1=$(date -d "$exp" +%s)
+d2=$(date -d "$now" +%s)
+exp2=$(( (d1 - d2) / 86400 ))
+if [[ "$exp2" = "0" ]]; then
+sed -i "s/### $user $exp/### $user $exp4/g" /etc/xray-mini/vless-direct.json
+sed -i "s/### $user $exp/### $user $exp4/g" /etc/xray-mini/vless-splice.json
+fi
+done
+systemctl restart xray-mini@vless-direct
+systemctl restart xray-mini@vless-splice
